851c15867807528d53cd23b60bfa067d3d3adad5
virgi <akunkuliahvirgi@gmail.com>
2026-06-25 22:15:33 +0700

    chore: force https scheme in production, trigger rebuild

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\View\Composers\CompanyProfileComposer;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', CompanyProfileComposer::class);
     }
 }
